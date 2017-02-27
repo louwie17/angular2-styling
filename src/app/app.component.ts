b09accea65410d261738f910597ec40764bfcf2a
Lourens Schep <lourens.schep@viacomcontractor.com>
2017-02-27 10:43:34 -0400

    Added the different styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+
+var template = `<div class="container">
+  <div class="nested">
+    <p>{{ name }}</p>
+    </div></div>`;
+
+var overlapStyle = `:host .nested { border: 1px solid black; }`;
+
+@Component({
+  selector: 'native',
+  template: template,
+  encapsulation: ViewEncapsulation.Native,
+  styles: [overlapStyle, `
+  .container { padding: 10px; }
+	p {
+	  color: red;
+	}
+	`]
+})
+export class NativeComponent { name = 'Native';}
+
+@Component({
+  selector: 'emulated',
+  template: template,
+  encapsulation: ViewEncapsulation.Emulated,
+  styles: [`
+  :host .nested { padding: 10px; }
+  :host /deep/ p {
+	  font-size: 20px;
+	}
+	`]
+})
+export class EmulatedComponent { name = 'emulated (default)';}
+
+@Component({
+  selector: 'none',
+  template: template,
+  encapsulation: ViewEncapsulation.None,
+  styles: [
+    `.container .nested { color: blue; }`
+  ]
+})
+export class NoneComponent { name = 'None'; }
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent {}
